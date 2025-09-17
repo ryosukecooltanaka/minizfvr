3e3e0ceeaa32b65b6e357afc065ca7c074aac22b
Ryosuke <ryosukecooltanaka@gmail.com>
2025-09-17 11:11:12 +0200

    separating out classes

--- dev/appdev_v0.py
+++ dev/appdev_v0.py
@@ -9,6 +9,7 @@ Some key things to get done here
 - Implement frame loading from camera, tail tracking algorithm, some sort of data logging
 - Worry about multiprocessing later
 - Split things into different files in the end
+- some degree of oop is inevitable...
 
 """
 
@@ -58,31 +59,15 @@ class HumanWindow(QMainWindow):
         
         # Create other widgets 
         self.stimulus_window = StimulusWindow()
-        self.camera_display = pg.GraphicsLayoutWidget() # sort of a container
-        self.angle_display = pg.GraphicsLayoutWidget() # a container
+        self.camera_panel = CameraPanel()
+        self.angle_panel = AnglePanel()
         self.control_panel = ControlPanel()
-
-        # Prepare camera view + tail axis
-        # register viewbox > image to the camera display
-        self.display_area = pg.ViewBox(invertY=True, lockAspect=True) # this graphics item implements easy scaling
-        self.camera_display.addItem(self.display_area) # add ViewBox to GraphicLayoutWidget
-        self.image_item = pg.ImageItem()
-        self.display_area.addItem(self.image_item) # add ImageItem to ViewBox
-        self.tail_standard = pg.LineSegmentROI([(10,10),(100,100)])
-        self.tail_standard.setPen(dict(color=(5, 40, 200), width=3))
-        self.display_area.addItem(self.tail_standard)
-
-        # Prepare tail angle plot
-        self.angle_plot = pg.PlotItem()
-        self.angle_display.addItem(self.angle_plot)
-        self.angle_line = pg.PlotDataItem()
-        self.angle_plot.addItem(self.angle_line)
 
         # Create vertical box layout. From top, we will show camera image, tail plot, and controls
         # Control box will require layouts of its own, but let's worry about that later
         layout = QVBoxLayout()
-        layout.addWidget(self.camera_display)
-        layout.addWidget(self.angle_display)
+        layout.addWidget(self.camera_panel)
+        layout.addWidget(self.angle_panel)
         layout.addWidget(self.control_panel)
         container.setLayout(layout)
         
@@ -90,30 +75,27 @@ class HumanWindow(QMainWindow):
         self.stimulus_window.show()
 
         # initialize camera
-        self.system = []
-        self.camera = []
-        self.initialize_camera()
+        self.camera = Camera()
         
         # Define timer for GUI
         self.timer = QTimer()
         self.timer.setInterval(50) # millisecond
         self.timer.timeout.connect(self.fetch_and_show_image) # define callback
         self.timer.start()
-        
-        
-    def initialize_camera(self):
-        self.system = PySpin.System.GetInstance()
-        self.camera = self.system.GetCameras()[0]
-        self.camera.Init()
-        self.camera.BeginAcquisition()
-        
+
     def fetch_and_show_image(self):
-        fetched_image = self.camera.GetNextImage()
-        if not fetched_image.IsIncomplete():
-            converted_image = np.array(fetched_image.GetData(), dtype='uint8').reshape((fetched_image.GetHeight(), fetched_image.GetWidth()))
-            fetched_image.Release()
-        self.image_item.setImage(converted_image)
-        self.angle_line.setData(np.arange(converted_image.shape[1]), np.mean(converted_image, axis=0))
+        img = self.camera.fetch_image()
+        self.camera_panel.set_image(img)
+        self.angle_panel.set_data(np.arange(img.shape[1]), np.mean(img, axis=0))
+
+    def closeEvent(self, event):
+        """
+        This will be called when the main window is closed.
+        Release resources for graceful exit.
+        """
+        self.timer.stop()
+        self.stimulus_window.close()
+        self.camera.close()
         
         
         
@@ -121,15 +103,85 @@ class StimulusWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        
+
+class CameraPanel(pg.GraphicsLayoutWidget):
+    """
+    This is the panel (widget) for the camera view
+    It holds image from camera + tail standard + tracked tail
+    """
+    def __init__(self):
+        super().__init__()
+        # This is the "GraphicsItem" that is added to the widget
+        self.display_area = pg.ViewBox(invertY=True, lockAspect=True)  # this graphics item implements easy scaling
+        self.fish_image_item = pg.ImageItem()
+        self.tail_standard = pg.LineSegmentROI([(10, 10), (100, 100)])
+        self.tail_standard.setPen(dict(color=(5, 40, 200), width=3))
+
+        # connect everything
+        self.addItem(self.display_area)
+        self.display_area.addItem(self.fish_image_item)
+        self.display_area.addItem(self.tail_standard)
+
+    def set_image(self, image):
+        """ Image update method """
+        self.fish_image_item.setImage(image)
+
+
+class AnglePanel(pg.GraphicsLayoutWidget):
+    """
+    This is the panel (widget) for the tail angle plot
+    """
+    def __init__(self):
+        super().__init__()
+        # Prepare tail angle plot item & data
+        self.angle_plot = pg.PlotItem()
+        self.angle_plot_data = pg.PlotDataItem()
+        # connect everything
+        self.addItem(self.angle_plot)
+        self.angle_plot.addItem(self.angle_plot_data)
+
+    def set_data(self, x, y):
+        """ Data update method """
+        self.angle_plot_data.setData(x, y)
+
+
 class ControlPanel(QWidget):
+    """
+    Hosts buttons, check boxes and such for experiment control
+    """
     def __init__(self):
         super().__init__()  
         layout = QHBoxLayout()
         for i in range(4):
             layout.addWidget(QLabel('dummy'))
         self.setLayout(layout)
-        
+
+class Camera():
+    def __init__(self):
+        self.system = PySpin.System.GetInstance()
+        self.camera = self.system.GetCameras()[0]
+        self.camera.Init()
+        self.camera.BeginAcquisition()
+
+    def fetch_image(self):
+        fetched_image = self.camera.GetNextImage()
+        if not fetched_image.IsIncomplete():
+            converted_image = np.array(fetched_image.GetData(), dtype='uint8').reshape(
+                (fetched_image.GetHeight(), fetched_image.GetWidth()))
+        fetched_image.Release()
+        return converted_image
+
+    def close(self):
+        """
+        Called from main window close event.
+        """
+        self.camera.EndAcquisition()
+        self.camera.DeInit()
+        del self.camera # this is required for system release
+        self.system.ReleaseInstance()
+
+
+
 def main():
     """ 
     This is what is called upon when you execute the file. 
